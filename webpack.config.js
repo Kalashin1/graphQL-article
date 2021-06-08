72e88eff740a9f89d7dc5301b86016f613518baf
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2021-06-08 15:54:38 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  target: 'node',
+  output: {
+    filename: 'index.js',
+    // path: path.resolve(__dirname + 'dist'),
+  },
+  stats: {
+    errorDetails: true
+  }
+}
